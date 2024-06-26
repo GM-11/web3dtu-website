5b978bad94ce61a9b6c068a124d7542380e2927d
GM-11 <mathuyrgopal@gmail.com>
2024-06-26 13:29:25 +0530

    decreased font size

--- src/Components/Departments.jsx
+++ src/Components/Departments.jsx
@@ -3,15 +3,18 @@ const Departments = () => {
     <div className="flex flex-col px-2 md:px-8 xl:px-0 gap-28 mt-24">
       {/* ----------------------- Software ----------------------- */}
       <div className="flex flex-col xl:flex-row">
-        <div className="bg-software-dev-image relative bg-no-repeat py-32 pl-5 md:pr-40 md:pl-8 md:py-72 rounded-l-[30px] xl:rounded-l-none rounded-r-[30px] bg-cover">
+        <div className="bg-software-dev-image relative bg-no-repeat py-28 pl-5 md:pr-40 md:pl-8 md:py-64 rounded-l-[30px] xl:rounded-l-none rounded-r-[30px] bg-cover">
           <div className="absolute top-0 left-0 right-0 bottom-0 rounded-l-[30px] xl:rounded-l-none bg-[#DB504A80] rounded-r-[30px] bg-opacity-35"></div>
           <div className="lg:text-7xl md:text-5xl text-4xl space-y-4 text-gray-100 z-10 relative font-black italic">
-            <div>SOFTWARE</div> <div>DEVELOPMENT</div>
+            <div>
+              SOFTWARE <br />
+              DEVELOPMENT
+            </div>
           </div>
         </div>
 
         <div className="flex py-2 md:gap-10 xl:gap-0 justify-between flex-col">
-          <div className="sm:text-4xl lg:text-5xl text-2xl text-white md:mt-5 xl:mt-0 xl:w-10/12 mx-auto flex gap-4 md:gap-9 2xl:gap-16 italic flex-col py-3 xl:text-right justify-around">
+          <div className="sm:text-3xl lg:text-4xl text-xl text-white md:mt-5 xl:mt-0 xl:w-10/12 mx-auto flex gap-4 md:gap-9 2xl:gap-16 italic flex-col py-3 xl:text-right justify-around">
             <div>
               The world of decentralized applications and smart contracts.
             </div>
@@ -22,24 +25,31 @@ const Departments = () => {
             </div>
           </div>
           <div className="flex xl:w-10/12 xl:mx-auto xl:flex-row-reverse">
-            <button className="rounded-[50px] md:px-16 px-8 border-2 md:border-4 border-[#DB504A] py-3 md:py-5 text-xl md:text-2xl bg-[#00336680] text-white italic font-black">
+            <a
+              href="https://chat.whatsapp.com/Fanj818GrxHGG5QCy1oXc0"
+              target="_blank"
+              className="rounded-[50px] md:px-16 px-8 border-2 md:border-4 border-[#DB504A] py-3 md:py-5 text-xl md:text-2xl bg-[#00336680] text-white italic font-black"
+            >
               JOIN NOW
-            </button>
+            </a>
           </div>
         </div>
       </div>
 
       {/* ----------------------- Core ----------------------- */}
       <div className="flex flex-col xl:flex-row-reverse">
-        <div className="bg-core-dev-image relative bg-no-repeat pl-4 py-32 pr-5 md:pl-40 md:pr-12 md:py-72 rounded-r-[30px] xl:rounded-r-none rounded-l-[30px] bg-cover">
+        <div className="bg-core-dev-image relative bg-no-repeat pl-4 py-32 pr-5 md:pl-40 md:pr-12 md:py-64 rounded-r-[30px] xl:rounded-r-none rounded-l-[30px] bg-cover">
           <div className="absolute top-0 left-0 right-0 bottom-0 rounded-r-[30px] xl:rounded-r-none bg-[#DB504A80] rounded-l-[30px] bg-opacity-35"></div>
           <div className="lg:text-7xl md:text-5xl text-4xl xl:text-right space-y-4 text-gray-100 z-10 relative font-black italic">
-            <div>CORE</div> <div>DEVELOPMENT</div>
+            <div>
+              CORE <br />
+              DEVELOPMENT
+            </div>
           </div>
         </div>
 
         <div className="flex py-2 md:gap-10 xl:gap-0 justify-between flex-col">
-          <div className="sm:text-4xl lg:text-5xl text-2xl text-white md:mt-5 xl:mt-0 xl:w-10/12 mx-auto flex gap-4 md:gap-9 2xl:gap-16 italic flex-col py-3 justify-around">
+          <div className="sm:text-3xl lg:text-4xl text-xl text-white md:mt-5 xl:mt-0 xl:w-10/12 mx-auto flex gap-4 md:gap-9 2xl:gap-16 italic flex-col py-3 justify-around">
             <div>Diving into the core technology of blockchain</div>
             <div>
               Where the actual data, actual transactions and actual magic
@@ -48,9 +58,13 @@ const Departments = () => {
             <div>Core engineering blockchain is being mastered here.</div>
           </div>
           <div className="flex xl:w-10/12 xl:mx-auto">
-            <button className="rounded-[50px] md:px-16 px-8 border-2 md:border-4 border-[#DB504A] py-3 md:py-5 text-xl md:text-2xl bg-[#00336680] text-white italic font-black">
+            <a
+              href="https://chat.whatsapp.com/LuUZ9cdZDta4wDXWT9J1LL"
+              target="_blank"
+              className="rounded-[50px] md:px-16 px-8 border-2 md:border-4 border-[#DB504A] py-3 md:py-5 text-xl md:text-2xl bg-[#00336680] text-white italic font-black"
+            >
               JOIN NOW
-            </button>
+            </a>
           </div>
         </div>
       </div>
